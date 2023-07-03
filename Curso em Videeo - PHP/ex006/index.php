c24452ca99092c893b9594da71ea4c387453dbd5
andreycoutt <contato.andreyyy@gmail.com>
2023-07-02 23:16:44 -0300

    Criando formulários

--- Curso em Videeo - PHP/ex006/index.php
+++ Curso em Videeo - PHP/ex006/index.php
@@ -3,9 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Criando formulários</title>
 </head>
 <body>
+    <header>
+        <h1>Apresente-se para nós</h1>
+    </header>
+    <section>
+        <form action="cad.php" method="post">
+            <label for="nome">Nome:</label>
+            <input type="text" name="nome" id="idnome" placeholder="Digite seu nome">
+            <label for="sobrenome">Sobrenome:</label>
+            <input type="text" name="sobrenome" id="idsobrenome" placeholder="Digite seu sobrenome">
+            <input type="submit" value="Enviar">
+        </form>
+    </section>
+
+    <?php 
     
+    
+    ?>
 </body>
 </html>
