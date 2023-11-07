b81e5a1fa8c5d44ba0ebfeef793dfae5879895fe
andreycoutt <contato.andreyyy@gmail.com>
2023-11-06 23:39:11 -0300

    .

--- Curso de PHP - João Ribeiro/002 - Variaveis/index.php
+++ Curso de PHP - João Ribeiro/002 - Variaveis/index.php
@@ -1,7 +1,19 @@
-<? php
-    $variavel = 100;
-    $var1 = 0;
-    $var_um = 0;
-    $varUm = 0;
-    $_1 = 0;
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <?php
+        $variavel = 100;
+        $var1 = 0;
+        $var_um = 0;
+        $varUm = 0;
+        $_1 = 0;
+    ?>
+</body>
+</html>
