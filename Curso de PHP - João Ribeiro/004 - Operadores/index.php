26cf96aa7a053e3117331d47ae28a75856f1f3ae
andreycoutt <contato.andreyyy@gmail.com>
2023-11-09 23:06:03 -0300

    .

--- Curso de PHP - João Ribeiro/004 - Operadores/index.php
+++ Curso de PHP - João Ribeiro/004 - Operadores/index.php
@@ -5,7 +5,7 @@ $variavel = 100;
 $variavel = "joao";
 
 $x = 4 + 2; # adição
-$x = 4 - 2; #subtração
+$x = 4 - 2; # subtração
 $x = 4 * 2; #multiplicação
 $x = 4 / 2; #divisão
 $x = 4 % 2; #modulo (resto da divisão)
